f25300aca062c25d0fa87bde4e328f400ffb05c9
arhan19926 <ali.arhan19926@gmail.com>
2025-01-25 20:47:38 +0530

    Added loader component with logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,36 @@
-import { useState } from 'react'
-import Lottie from 'lottie-react'
-import './App.css'
-import AnimRocket from './assets/animations/AnimRocket.json'
+import { useEffect, useState } from "react";
+import Lottie from "lottie-react";
+import "./App.css";
+import AnimRocket from "./assets/animations/AnimRocket.json";
+import Loader from "./components/loader/loader";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [isloading, setIsloading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsloading(false);
+    }, 2000);
+  },[]);
 
   return (
     <>
-      <div className='anim'>
-      <Lottie animationData={AnimRocket}></Lottie>
-      </div>
-      <h1>Vite + React starter</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      {isloading ? (
+        <Loader />
+      ) : (
+        <>
+          <div className="anim">
+            <Lottie animationData={AnimRocket}></Lottie>
+          </div>
+          <h1>Vite + React starter</h1>
+          <div className="card">
+            <button onClick={() => setCount((count) => count + 1)}>
+              count is {count}
+            </button>
+          </div>
+        </>
+      )}
     </>
-  )
+  );
 }
-
-export default App
+export default App;
